50ca5a1e3cd2314f10a389798c7321e7b3ea7a49
Shusnuke Oba <obashun22@yahoo.co.jp>
2023-04-27 17:38:56 +0900

    Update 環境変数導入

--- src/api/ApiClient.ts
+++ src/api/ApiClient.ts
@@ -1,16 +1,15 @@
-const API_ENDPOINT = "http://localhost:4000/whisper/transcribe";
+const API_ENDPOINT = `${process.env.REACT_APP_API_HOST}/whisper/transcribe`;
 
 export class ApiClient {
   public async translateToSrt(file: File) {
-    const postData = new FormData(); // FormDataでPOSTしないとバックエンドで受け取れない？
+    const postData = new FormData();
     postData.append("file", file);
     return await fetch(API_ENDPOINT, {
       method: "POST",
-      // モックサーバの場合はコメントアウト
-      // body: postData,
-      // headers: new Headers({
-      //   "Content-Type": "multipart/form-data",
-      // }),
+      body: postData,
+      headers: new Headers({
+        "Content-Type": "multipart/form-data",
+      }),
       mode: "cors",
     })
       .then((response) => response.blob())
@@ -18,7 +17,6 @@ export class ApiClient {
         const file = new File([blob], "transcribe.srt", {
           type: "text/plain",
         });
-        file.text().then((text) => console.log(text));
         return file;
       })
       .catch((err) => {
